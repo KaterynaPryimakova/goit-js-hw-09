597b5007b341bd2f841fa7ef2f1ff55c82d52846
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-24 22:56:30 +0300

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+import { getRandomHexColor } from './random-color';
 
+const background = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChanging);
+
+let changeId;
+
+function changeColor() {
+  background.style.backgroundColor = '#337763';
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  changeId = setInterval(() => {
+    background.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChanging() {
+  clearInterval(changeId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
