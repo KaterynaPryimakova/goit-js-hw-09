4fac1ff41f4c8ab131a21319d172da49a3b56f00
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-27 15:05:06 +0300

    third task done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,43 @@
 const form = document.querySelector('.form');
 const inputAmount = document.querySelector('[name="amount"]');
+const inputFirstDelay = document.querySelector('[name="delay"]');
+const inputDelayStep = document.querySelector('[name="step"]');
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
   event.preventDefault();
-  const amount = inputAmount.value;
-  console.log(amount);
-  setInterval(createPromise(), amount);
+  const amount = Number(inputAmount.value);
+  const step = Number(inputDelayStep.value);
+  const firstDelay = Number(inputFirstDelay.value);
+  let delay = firstDelay;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const step = Number(inputDelayStep.value);
+  const promise = new Promise((resolve, reject) => {
+    // Asynchronous operation
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
