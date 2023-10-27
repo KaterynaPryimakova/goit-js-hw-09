e4052750e0ac9357644b9ecc98c86633faae9e99
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-27 12:01:55 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+const form = document.querySelector('.form');
+const inputAmount = document.querySelector('[name="amount"]');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const amount = inputAmount.value;
+  console.log(amount);
+  setInterval(createPromise(), amount);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
